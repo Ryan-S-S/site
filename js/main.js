63507321bc27fb008463ba732b7387dbb42328ee
Ryan SS <166767290+Ryan-S-S@users.noreply.github.com>
2025-03-14 13:15:01 -0300

    Funções basicas

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 import { db } from "./firebase-config.js";
 import { doc, setDoc, getDoc, updateDoc, deleteDoc, deleteField } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
-async function salvarDados() {
-    await setDoc(doc(db, "usuarios", "123"), {
-        nome: "João",
-        idade: 25
-    });
-    console.log("Dados salvos!");
+async function salvarUsuario(id, nome, idade) {
+    await setDoc(doc(db, "usuarios", id), { 
+        nome, 
+        idade });
+    console.log("Usuário salvo:", nome);
 }
 
-async function lerDados() {
-    const docSnap = await getDoc(doc(db, "usuarios", "123"));
+async function lerDados(id) {
+    const docSnap = await getDoc(doc(db, "usuarios", id));
     if (docSnap.exists()) {
         console.log("Dados:", docSnap.data());
     } else {
@@ -18,5 +17,21 @@ async function lerDados() {
     }
 }
 
-salvarDados();
-lerDados();
+async function apagarDocumento(id) {
+    await deleteDoc(doc(db, "usuarios", id));
+    console.log("Documento apagado!");
+}
+
+async function listarDocumentos(colecao) {
+    const querySnapshot = await getDocs(collection(db, colecao));
+
+    if (querySnapshot.empty) {
+        console.log(`A coleção "${colecao}" está vazia.`);
+        return;
+    }
+
+    console.log(`Documentos na coleção "${colecao}":`);
+    querySnapshot.forEach((doc) => {
+        console.log(`ID: ${doc.id} →`, doc.data());
+    });
+}
